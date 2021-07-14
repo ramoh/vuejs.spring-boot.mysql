8998e134f2504f5e97eea287373cb8ffd2a24d76
Rajesh Mohanty <mohantyrajesh1984@gmail.com>
2021-07-14 14:33:52 +0530

    create back-end scaffold

---
+++ src/test/java/com/rajesh/taskagile/app/TaskAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajesh.taskagile.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAgileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
